d481c69773cd8e15a30f75a90ea070d5cac39c7e
CasterraDev <casterradev@gmail.com>
2023-11-02 03:13:48 -0400

    Added active path styling

--- src/components/SideLinkCard.tsx
+++ src/components/SideLinkCard.tsx
@@ -1,25 +1,32 @@
+"use client"
 import Link from "next/link"
 import styles from "../css/SideLinkCard.module.css"
 import Card from "./Card"
 import LogoutBtn from "./LogoutBtn"
-import { getServerSession } from "next-auth"
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"
 import LoginBtn from "./LoginBtn"
+import { useSession } from "next-auth/react"
+import { usePathname } from "next/navigation"
 
-export default async function SideLinkCard() {
-    const session = await getServerSession(authOptions);
-    console.log("Session: " + session)
-    console.log("SessUser " + session?.user)
+export default function SideLinkCard() {
+    const { data: session, status } = useSession();
+    const path: string = usePathname();
+
+    let profileLink = "/login";
+    if (session) {
+        profileLink = `/profile/${session.user.username}`
+    }
+
     return (
         <Card>
-            <Link href="/" className={`${styles.link} ${styles.linkActive}`}>
+
+            <Link href="/" className={`${styles.link} ${path == "/" && styles.linkActive}`}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                 </svg>
                 <p>Home</p>
             </Link>
 
-            <Link href="search" className={styles.link}>
+            <Link href="/search" className={`${styles.link} ${path == "/search" && styles.linkActive}`}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                 </svg>
@@ -27,28 +34,26 @@ export default async function SideLinkCard() {
             </Link>
 
 
-            <Link href="saved" className={styles.link}>
+            <Link href="/saved" className={`${styles.link} ${path == "/saved" && styles.linkActive}`}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
                 </svg>
                 <p>Saved Media</p>
             </Link>
 
-            <Link href="notifications" className={styles.link}>
+            <Link href="/notifications" className={`${styles.link} ${path == "/notifications" && styles.linkActive}`}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
                 </svg>
                 <p>Notifications</p>
             </Link>
 
-            {session &&
-                <Link href="" className={styles.link}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-                    </svg>
-                    <p>Profile</p>
-                </Link>
-            }
+            <Link href={profileLink} className={`${styles.link} ${path == `/profile/${session?.user.username}` && styles.linkActive}`}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                </svg>
+                <p>Profile</p>
+            </Link>
 
             {session ? <LogoutBtn /> : <LoginBtn />}
         </Card>
